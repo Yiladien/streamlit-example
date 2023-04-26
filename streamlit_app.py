c0591e34a8efeb1ec3c81ede62b6e0d8ba18cec2
Yiladien <100401790+Yiladien@users.noreply.github.com>
2023-04-25 23:06:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,5 +72,14 @@ df = pd.DataFrame(jsonData2)
 st.dataframe(df)
 st.experimental_data_editor(df)
 df2 = df.groupby(['Category'])['Category'].count()
-# st.table(df2)
+st.table(df2)
 st.bar_chart(df2, x=None, y=None)
+
+
+import numpy as np
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=["a", "b", "c"])
+
+st.bar_chart(chart_data)
