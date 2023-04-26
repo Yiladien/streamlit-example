3ab8b2fdf876c662bc1a9b078f2ff3e6c6e6b568
Yiladien <100401790+Yiladien@users.noreply.github.com>
2023-04-25 20:17:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-# from collections import namedtuple
-# import altair as alt
-# import math
-# import pandas as pd
-# import streamlit as st
+from collections import namedtuple
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
 
 """
 # Welcome to Streamlit!
