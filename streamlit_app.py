5518b45525022d33dc62066fe16359fa1509016d
Yiladien <100401790+Yiladien@users.noreply.github.com>
2023-04-25 21:45:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,7 +57,9 @@ st.json(jsonData, expanded=False)
 
 jsonData2 = []
 for arr1 in jsonData:
+  print(arr1)
   info = arr1['info']
+  print(info)
   jsonData2.append(info)
 st.json(jsonData2, expanded=False)
 
